60cb516fe66487a1ae5f1996142535590452d9ab
Jaime García <jaime.garcia@iesdonana.org>
2024-10-10 21:09:32 +0200

    Buscar por código

--- auxiliar.php
+++ auxiliar.php
@@ -3,3 +3,8 @@ function conectar()
 {
     return new PDO('pgsql:host=localhost;dbname=datos', 'datos', 'datos');
 }
+
+function obtener_get($par)
+{
+    return isset($_GET[$par]) ? trim($_GET[$par]) : null;
+}
--- departamentos.php
+++ departamentos.php
@@ -1,19 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Departamentos</title>
 </head>
+
 <body>
     <?php
     require 'auxiliar.php';
-    
+
+    // if (isset($_GET['codigo'])) {
+    //     $codigo = trim($_GET['codigo']);
+    // } else {
+    //     $codigo = null;
+    // }
+
+    $codigo = obtener_get('codigo');
     $pdo = conectar();
-    $stmt = $pdo->query('   SELECT *
-                            FROM departamentos
-                            ORDER BY codigo');
+
+    if ($codigo == null || $codigo == '') {
+        $where = 'true';
+        $execute = [];
+    } else {
+        $where = 'codigo = :codigo';
+        $execute = [':codigo' => $codigo];
+    }
+    $stmt = $pdo->prepare(" SELECT *
+                                FROM departamentos
+                                WHERE $where
+                                ORDER BY codigo");
+    $stmt->execute($execute);
     ?>
+    <form action="" method="get">
+        <label for="codigo">Código:
+            <input type="text" name="codigo" value="<?= $codigo ?>">
+        </label>
+        <button type="submit">Buscar</button>
+    </form>
+    <br>
     <table border="1">
         <thead>
             <th>Código</th>
@@ -38,4 +64,5 @@
     //     var_dump($fila);
     // } -->
 </body>
+
 </html>
