9a1401dc1b669cb8ccc2ea4e2fb0f258225a25c8
Ricardo Pérez <ricpelo@gmail.com>
2024-10-14 21:16:59 +0200

    Haciendo borrado

---
+++ hacer_borrado.php
@@ -0,0 +1,9 @@
+<?php
+require 'auxiliar.php';
+
+$id = obtener_post('id');
+$pdo = conectar();
+$stmt = $pdo->prepare('DELETE FROM departamentos
+                             WHERE id = :id');
+$stmt->execute([':id' => $id]);
+header('Location: departamentos.php');
