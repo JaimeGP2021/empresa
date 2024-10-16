fd517741937ba254842e9faf36a97159e57f7063
Jaime García <jaime.garcia@iesdonana.org>
2024-10-16 20:47:26 +0200

    inserción de departamentos con mensajes de error y validaciones

--- auxiliar.php
+++ auxiliar.php
@@ -24,7 +24,7 @@ function volver_departamentos()
     header('Location: departamentos.php');
 }
 
-function obtener_departamento($id, ?PDO $pdo = null, $bloqueo = false): array|false
+function departamento_por_id($id, ?PDO $pdo = null, $bloqueo = false): array|false
 {
     $pdo = $pdo ?? conectar();
     $sql = 'SELECT * FROM departamentos WHERE id = :id';
@@ -34,4 +34,65 @@ function obtener_departamento($id, ?PDO $pdo = null, $bloqueo = false): array|fa
     $stmt = $pdo->prepare($sql);
     $stmt->execute([':id' => $id]);
     return $stmt->fetch();
+}
+
+function departamento_por_codigo($codigo, ?PDO $pdo = null, $bloqueo = false): array|false
+{
+    $pdo = $pdo ?? conectar();
+    $sql = 'SELECT * FROM departamentos WHERE codigo = :codigo';
+    if ($bloqueo) {
+        $sql.= ' FOR UPDATE';
+    }
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([':codigo' => $codigo]);
+    return $stmt->fetch();
+}
+
+function anyadir_error($par, $mensaje, &$errores)
+{
+    if (!isset($errores[$par])) {
+        $errores[$par] = [];
+    }
+    $errores[$par][] = $mensaje;
+}
+
+function comprobar_codigo($codigo, &$errores, ?PDO $pdo = null)
+{
+    $pdo = $pdo ?? conectar();
+    if ($codigo === '') {
+        anyadir_error('codigo', 'El código no puede estar vacío', $errores);
+    } elseif (mb_strlen($codigo) > 2) {
+        anyadir_error('codigo', 'El código es demasiado largo', $errores);
+    } elseif (departamento_por_codigo($codigo, $pdo)) {
+        anyadir_error('codigo', 'Ese departamento ya existe', $errores);
+    } 
+}
+
+function comprobar_denominacion($denominacion, &$errores, ?PDO $pdo = null)
+{
+    $pdo = $pdo ?? conectar();
+    if ($denominacion === '') {
+        anyadir_error('denominacion', 'La denominación no puede estar vacía', $errores);
+    } elseif (mb_strlen($denominacion) > 255) {
+        anyadir_error('denominacion', 'La denominación es demasiado larga', $errores);
+    } 
+}
+
+function comprobar_localidad($localidad, &$errores, ?PDO $pdo = null)
+{
+    $pdo = $pdo ?? conectar();
+    if ($localidad === '') {
+        $localidad = null;
+    } elseif (mb_strlen($localidad) > 255) {
+        anyadir_error('localidad', 'La denominación es demasiado larga', $errores);
+    } 
+}
+
+function mostrar_errores($errores)
+{
+    foreach($errores as $par => $mensajes) {
+        foreach($mensajes as $mensaje) { ?>
+            <h2><?= $mensaje ?></h2><?php
+        }
+    }
 }
--- insertar.php
+++ insertar.php
@@ -1,26 +1,61 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insertar un nuevo departamento</title>
 </head>
+
 <body>
-    <form action="hacer_insercion.php" method="post">
+    <?php
+    require 'auxiliar.php';
+
+    $codigo = obtener_post('codigo');
+    $denominacion = obtener_post('denominacion');
+    $localidad = obtener_post('localidad');
+    $pdo = conectar();
+
+    if (isset($codigo, $denominacion, $localidad)) {
+        $errores = [];
+        comprobar_codigo($codigo, $errores, $pdo);
+        comprobar_denominacion($denominacion, $errores, $pdo);
+        comprobar_localidad($localidad, $errores, $pdo);
+
+
+        if (!empty($errores)) {
+            mostrar_errores($errores);
+        }else {
+            $stmt = $pdo->prepare(' INSERT INTO departamentos 
+                                    (codigo, denominacion, localidad)
+                                    VALUES (:codigo, :denominacion, :localidad)');
+            $stmt->execute([
+                ':codigo' => $codigo,
+                ':denominacion' => $denominacion,
+                ':localidad' => $localidad,
+            ]);
+            setcookie('exito', 'El departamento se ha insertado correctamente');
+            volver_departamentos();
+            return;
+        }
+    }
+    ?>
+    <form action="" method="post">
         <label for="codigo">Código:
-            <input type="text" name="codigo" id="codigo">
+            <input type="text" name="codigo" id="codigo" value="<?= $codigo?>">
         </label>
         <br>
         <label for="denominacion">Denominación:
-            <input type="text" name="denominacion" id="denominacion">
+            <input type="text" name="denominacion" id="denominacion" value="<?= $denominacion?>">
         </label>
         <br>
         <label for="localidad">Localidad:
-            <input type="text" name="localidad" id="localidad">
+            <input type="text" name="localidad" id="localidad" value="<?= $localidad?>">
         </label>
         <br>
         <button type="submit">Insertar</button>
         <a href="departamentos.php">Volver</a>
     </form>
 </body>
+
 </html>
