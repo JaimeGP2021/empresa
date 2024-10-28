a4dffbea8230bb8cc59ff2fc24c025b72f1dbb06
Jaime García <jaime.garcia@iesdonana.org>
2024-10-28 21:25:39 +0100

    tailwind metío

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.php", 
+    "auxiliar/*.{html,js,php}",
+    "departamentos/*.{html,js,php}",
+    "empleados/*.{html,js,php}",
+    "usuarios/*.{html,js,php}"
+  ],
+    theme: {
+    extend: {},
+  },
+  plugins: [],
+};
