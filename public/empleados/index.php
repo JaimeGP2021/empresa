b0ddc8f127096e141d03f7504178156a0dc73a36
Jaime García <jaime.garcia@iesdonana.org>
2024-11-04 19:44:30 +0100

    Header at empleados

--- public/empleados/index.php
+++ public/empleados/index.php
@@ -1,17 +1,17 @@
 <?php session_start() ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/output.css">
     <title>Empleados</title>
 </head>
 <body>
     <?php
     require '../../src/auxiliar.php';
+    require '../../src/views/_menu.php';
     
-    cabecera();
-
     $pdo = conectar();
     $stmt = $pdo->query('   SELECT e.*, d.codigo, d.denominacion
                             FROM empleados e LEFT JOIN departamentos d
@@ -42,5 +42,6 @@
         </tbody>
     </table>
     <a href="insertar_empleado.php">Insertar un nuevo empleado</a>
+    <script src="/js/flowbite/flowbite.js"></script>
 </body>
 </html>
